96920f6cfa057a0e61647598aa0cb1115e231c2a
Dev <dev@email.com>
2022-02-22 13:24:08 +1000

    End lecture 120

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Server...");
+// Configuration
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+// Initialisation
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Welcome to the support desk API' });
+});
+
+// Listen
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
