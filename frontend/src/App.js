e5afc7c42751612f7fde3960b132edb7af13d446
Dev <dev@email.com>
2022-02-23 13:47:57 +1000

    End lecture 128

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
+// Modules
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+// Components
+import Header from './components/Header';
+
 function App() {
-  return <div>MERN Application Project</div>;
+  return (
+    <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
